9519cf521657ad37adafe7c723fa047b82cc3488
Hans Danny Muñoz Ramos <44986982+DonHansCL@users.noreply.github.com>
2024-11-22 12:31:18 -0300

    Update honorarioRoutes.js

--- backend/routes/honorarioRoutes.js
+++ backend/routes/honorarioRoutes.js
@@ -84,7 +84,7 @@ router.post('/upload', upload.single('file'), verifyToken, checkRole(['ADMIN', '
       // Leer y procesar el CSV
       await new Promise((resolve, reject) => {
         fs.createReadStream(req.file.path)
-          .pipe(csv({ separator: ';', headers: true, trim: true }))
+           .pipe(csv({ separator: ';' }))
           .on('data', (data) => {
             resultados.push({ ...data, fila });
             fila++;
